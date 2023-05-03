4b4b7550c972a4d4f75364ef62346f2da58d500f
Renzo Battaglino <naghybusiness@gmail.com>
2023-05-03 16:46:15 -0300

    Nav agregado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Cards from './components/Cards/Cards.jsx';
-import SearchBar from './components/SearchBar/SearchBar.jsx';
+import Nav from './components/Nav/Nav.jsx';
 import characters from './data/data.js';
 
 function App() {
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         <Nav onSearch={(characterID) => window.alert(characterID)} />
          <Cards characters={characters} />
       </div>
    );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,11 @@
+import style from './SearchBar.module.css';
+
 export default function SearchBar(props) {
-   console.log(props.name)
-   return (
-      <div>
-         <input type='search' />
-         <button onClick={() => props.onSearch(props.characterID)}>Agregar</button>
-      </div>
-   );
-}
+    console.log(props.name)
+    return (
+       <div>
+          <input type='search' />
+          <button onClick={() => props.onSearch(props.characterID)}>Agregar</button>
+       </div>
+    );
+ }
