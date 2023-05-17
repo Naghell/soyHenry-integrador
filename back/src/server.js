720d086476ad42e4bfd08b4b9e7988db3f98d908
Renzo Battaglino <naghybusiness@gmail.com>
2023-05-17 12:42:30 -0300

    test

---
+++ back/src/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const characters = require('./utils/data');
+const PORT = 3001;
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    if (req.url.includes('rickandmorty/character')) {
+        const id = req.url.split('/').at(-1);
+        const character = characters.find((character) => {
+            return character.id === Number(id);
+        });
+
+        res.writeHead(200, {"Content-Type": "application/json"}).end(JSON.stringify(character));
+    }
+}).listen(PORT);
