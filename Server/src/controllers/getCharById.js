d966341893b0629b14241609728eac7469222ddc
Renzo Battaglino <naghybusiness@gmail.com>
2023-05-23 10:40:30 -0300

    Async await

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,32 +1,28 @@
-const axios = require('axios');
-const URL = 'https://rickandmortyapi.com/api/character'
+const axios = require("axios");
+const URL = "https://rickandmortyapi.com/api/character";
 
-const getCharById = (req, res) => {
+const getCharById = async (req, res) => {
+  try {
+
     const { id } = req.params;
-    axios(`${URL}/${id}`)
-    .then(response => response.data)
-    .then(({status, name, species, origin, gender, image}) => {
-        if(name) {
-            const character = {
-                id,
-                name,
-                species,
-                origin,
-                status,
-                gender,
-                image
-            };
+    const { data } = await axios(`${URL}/${id}`);
+    if (!data.name) throw Error(`ID: ${id} not found`);
+    const character = {
+      id: data.id,
+      name: data.name,
+      species: data.species,
+      origin: data.origin,
+      status: data.status,
+      gender: data.gender,
+      image: data.image
+    };
+    return res.status(200).json(character);
 
-            return res.status(200).json(character);
-        }
-
-        return res.status(404).send('Not found');
-    })
-    .catch(error => {
-        res.status(500).send(error.message);
-    })
-}
+  } catch (error) {
+    return error.message.includes('ID') ? res.status(400).send(error.message) : res.status(500).send(error.response.data.error);
+  }
+};
 
 module.exports = {
-    getCharById
-}
+  getCharById,
+};
--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -2,16 +2,19 @@ let myFav = [];
 
 const postFav = (req, res) => {
     const character = req.body;
-    myFav.push(character);
-    
+    const characterExists = myFav.some((favCharacter) => favCharacter.id === character.id);
+  
+    if (!characterExists) {
+        myFav.push(character);
+    }
     return res.status(200).json(myFav);
-}
+  };
 
 const deleteFav = (req, res) => {
     const { id } = req.params;
-    const myFav = myFav.filter(character => character.id !== +id);
+    const myFavFilter = myFav.filter(character => character.id !== id);
 
-    return res.status(200).json(myFav);
+    return res.status(200).json(myFavFilter);
 }
 
 module.exports = {
